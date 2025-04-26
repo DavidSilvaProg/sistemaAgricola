be217693eebda8304158e688481bae603cb0522b
DavidSilvaProg <davidsilva.prog@gmail.com>
2025-04-26 12:28:23 -0300

    Corrigindo bugs na conexao com o banco, agora ele sempre cria um nova conexao. Criado funcoes decoradas para login e bivel de usuario

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from datetime import datetime
 from flask import Flask, request, redirect, url_for, render_template, session, flash
 from werkzeug.security import check_password_hash, generate_password_hash
+from functools import wraps
 
 app = Flask(__name__)
 load_dotenv()
@@ -18,16 +19,16 @@ class Database:
             "password": os.getenv("DB_PASSWORD"),
             "database": os.getenv("DB_NAME")
         }
-        self.conexao = mysql.connector.connect(**self.db_config)
+        self.conexao = None
 
-    def _reconectar(self):
+    def _conectar(self):
         self.conexao = mysql.connector.connect(**self.db_config)
 
     def execute(self, query, params=None, fetch=False, varios=False):
         cursor = None
         try:
-            if not self.conexao.is_connected():
-                self._reconectar()
+            self._conectar()
+
             cursor = self.conexao.cursor(dictionary=True)
 
             if varios:
@@ -45,12 +46,16 @@ class Database:
             return cursor.lastrowid
 
         except mysql.connector.Error as e:
-            self.conexao.rollback()
+            if self.conexao:
+                self.conexao.rollback()
             print(f"Erro no banco de dados: {e}")
             return None
         finally:
             if cursor:
                 cursor.close()
+
+            if self.conexao:
+                self.conexao.close()
 
     def close(self):
         self.conexao.close()
@@ -248,23 +253,44 @@ class Autenticacao:
     def __init__(self):
         self.db = Database()
 
-    def logar(self, email, senha):
+    def buscar_usuario_por_email(self, email):
         query = "SELECT * FROM usuarios WHERE email_usuario = %s"
-        usuario = self.db.execute(query, (email,), fetch=True)
-        for usuario in usuario:
-            if usuario and check_password_hash(usuario["senha_usuario"], senha):
-                session["user_id"] = usuario["id_usuario"]
-                session["user_nome"] = usuario["nome_usuario"]
-                session["nivel"] = usuario["nivel_usuario"]
-                return True
-            else:
-                return False
+        usuarios = self.db.execute(query, (email,), fetch=True)
+        return usuarios[0] if usuarios else None
 
-    def verificar_login(self):
-        if "user_id" not in session:
-            return redirect(url_for("login"))
+    def iniciar_sessao(self, usuario):
+        session["user_id"] = usuario["id_usuario"]
+        session["user_nome"] = usuario["nome_usuario"]
+        session["nivel"] = usuario["nivel_usuario"]
 
 
+    def logar(self, email, senha):
+        usuario = self.buscar_usuario_por_email(email)
+        if usuario and check_password_hash(usuario["senha_usuario"], senha):
+            self.iniciar_sessao(usuario)
+            return True
+        return False
+
+    def login_required(self, func):
+        @wraps(func)
+        def decorated_view(*args, **kwargs):
+            if "user_id" not in session:
+                return redirect(url_for("login"))
+            return func(*args, **kwargs)
+        return decorated_view
+
+    def nivel_required(self, nivel_necessario):
+        def decorator(func):
+            @wraps(func)
+            def decorated_view(*args, **kwargs):
+                if "nivel" not in session or session["nivel"] != nivel_necessario:
+                    return redirect(url_for("solicitacoesCompras"))
+                return func(*args, **kwargs)
+            return decorated_view
+        return decorator
+
+    def logout(self):
+        session.clear()
 
 
 solicitacao_service = Solicitacao()
@@ -272,11 +298,9 @@ autenticacao_service = Autenticacao()
 
 #Página inicial
 @app.route('/')
+@autenticacao_service.login_required
 def index():
-    if "user_id" not in session:
-        return redirect(url_for("login"))
-    else:
-        return redirect(url_for("solicitacoesCompra"))
+    return redirect(url_for("solicitacoesCompra"))
 
 #login de usuário
 @app.route("/login", methods=['GET','POST'])
@@ -292,105 +316,86 @@ def login():
 
 #registro de novos usuários
 @app.route('/cadastrarUsuario', methods=['GET', 'POST'])
+@autenticacao_service.login_required
+@autenticacao_service.nivel_required('administrador')
 def cadastrarUsuario():
-    if "user_id" in session:
-        if session['nivel'] == "administrador":
-            if request.method == "POST":
-                nome = request.form["nome"]
-                email = request.form["email"]
-                senha = generate_password_hash(request.form["senha"])
-                nivel = request.form['nivel']
-                solicitacao_service.cadastrar_usuario(nome, email, senha, nivel)
-            return render_template("cadastrarUsuario.html", action_url=url_for('cadastrarUsuario'))
-        else:
-            return redirect(url_for('solicitacoesCompra'))
-    else:
-        return redirect(url_for("login"))
+    if request.method == "POST":
+        nome = request.form["nome"]
+        email = request.form["email"]
+        senha = generate_password_hash(request.form["senha"])
+        nivel = request.form['nivel']
+        solicitacao_service.cadastrar_usuario(nome, email, senha, nivel)
 
+    return render_template("cadastrarUsuario.html", action_url=url_for('cadastrarUsuario'))
+
+@autenticacao_service.login_required
+@autenticacao_service.nivel_required('administrador')
 @app.route('/gravaEditarUsuario/<int:id>', methods=['POST'])
 def gravaEditarUsuario(id):
-    if "user_id" in session:
-        if session['nivel'] == "administrador":
-            if request.method == "POST":
-                nome = request.form["nome"]
-                email = request.form["email"]
-                senha = generate_password_hash(request.form["senha"])
-                nivel = request.form['nivel']
-                solicitacao_service.editar_usuario(id, nome, email, senha, nivel)
-            return render_template("cadastrarUsuario.html")
-        else:
-            return redirect(url_for('solicitacoesCompra'))
-    else:
-        return redirect(url_for("login"))
+    if request.method == "POST":
+        nome = request.form["nome"]
+        email = request.form["email"]
+        senha = generate_password_hash(request.form["senha"])
+        nivel = request.form['nivel']
+        solicitacao_service.editar_usuario(id, nome, email, senha, nivel)
+    return render_template("cadastrarUsuario.html")
+
 
 @app.route('/editarUsuario/<int:id>')
+@autenticacao_service.login_required
+@autenticacao_service.nivel_required('administrador')
 def editarUsuario(id):
-    if "user_id" in session:
-        if session['nivel'] == "administrador":
-            usuario_resultado = solicitacao_service.buscar_usuarios(id, unica=True)
-            usuario = usuario_resultado[0] if usuario_resultado else None
-            return render_template('cadastrarUsuario.html', usuario=usuario, action_url=url_for('gravaEditarUsuario', id=id))
-        else:
-            return redirect(url_for('solicitacoesCompra'))
-    else:
-        return redirect(url_for("login"))
+    usuario_resultado = solicitacao_service.buscar_usuarios(id, unica=True)
+    usuario = usuario_resultado[0] if usuario_resultado else None
+    return render_template('cadastrarUsuario.html', usuario=usuario, action_url=url_for('gravaEditarUsuario', id=id))
 
 @app.route('/excluirUsuario/<int:id>')
+@autenticacao_service.login_required
+@autenticacao_service.nivel_required('administrador')
 def excluirUsuario(id):
-    if "user_id" in session:
-        if session['nivel'] == "administrador":
-            solicitacao_service.excluir_usuario(id)
-            return redirect(url_for("usuarios"))
-        else:
-            return redirect(url_for('solicitacoesCompra'))
-    else:
-        return redirect(url_for("login"))
+    solicitacao_service.excluir_usuario(id)
+    return redirect(url_for("usuarios"))
 
 @app.route('/usuarios')
+@autenticacao_service.login_required
+@autenticacao_service.nivel_required('administrador')
 def usuarios():
-    if "user_id" in session:
-        usuarios = solicitacao_service.buscar_usuarios()[::-1] #inverte a lista
-        return render_template('usuarios.html', usuarios = usuarios, nome=session['user_nome'])
-    else:
-        return redirect(url_for("login"))
+    usuarios = solicitacao_service.buscar_usuarios()[::-1] #inverte a lista
+    return render_template('usuarios.html', usuarios = usuarios, nome=session['user_nome'])
+
 
 #logout
 @app.route('/logout')
 def logout():
-    session.clear()
+    autenticacao_service.logout()
     return redirect(url_for('login'))
 
 #Página de solicitações de compra
 @app.route('/solicitacoesCompra')
+@autenticacao_service.login_required
 def solicitacoesCompra():
-    if "user_id" in session:
-        solicitacoes = solicitacao_service.buscar_solicitacoes()[::-1] #inverte a lista
-        return render_template('solicitacoesCompra.html', pedidos = solicitacoes, nome=session['user_nome'])
-    else:
-        return redirect(url_for("login"))
+    solicitacoes = solicitacao_service.buscar_solicitacoes()[::-1] #inverte a lista
+    return render_template('solicitacoesCompra.html', pedidos = solicitacoes, nome=session['user_nome'])
 
 #Página de detalhes da solicitacao
 @app.route('/detalhesSolicitacao/<int:id>')
+@autenticacao_service.login_required
 def detalhesSolicitacao(id):
-    if "user_id" in session:
-        solicitacao = solicitacao_service.buscar_solicitacoes(id, unica=True)
-        produtos = solicitacao_service.buscar_produtos(id)
-        return render_template('detalhesSolicitacao.html', solicitacao=solicitacao, produtos=produtos)
-    else:
-        return redirect(url_for("login"))
+    solicitacao = solicitacao_service.buscar_solicitacoes(id, unica=True)
+    produtos = solicitacao_service.buscar_produtos(id)
+    return render_template('detalhesSolicitacao.html', solicitacao=solicitacao, produtos=produtos)
 
 
 #Página de cadastro de solicitações de compras, carrega dados de produtos e setores
 @app.route('/cadastroSolicitacao')
+@autenticacao_service.login_required
 def cadastroSolicitacao():
-    if "user_id" in session:
-        setores = solicitacao_service.buscar_setores()
-        return render_template('cadastroSolicitacao.html', setores=setores)
-    else:
-        return redirect(url_for("login"))
+    setores = solicitacao_service.buscar_setores()
+    return render_template('cadastroSolicitacao.html', setores=setores)
 
 #Recebe as solicitação de compra do formulário e grava no banco
 @app.route('/cadastrarSolicitacao', methods=['POST'])
+@autenticacao_service.login_required
 def cadastrarSolicitacao():
     nome = request.form['nome']
     setor = request.form['setor']
@@ -406,77 +411,56 @@ def cadastrarSolicitacao():
     return redirect(url_for('cadastroSolicitacao'))
 
 @app.route('/editarStatusSolicitacao/<int:id>', methods=['POST'])
+@autenticacao_service.login_required
+@autenticacao_service.nivel_required('administrador')
 def editarStatusSolicitacao(id):
-    if "user_id" in session:
-        if session['nivel'] == "administrador":
-            status = request.form['status']
-            solicitacao_service.editarStatusSolicitacao(status, id)
-            return redirect(url_for('solicitacoesCompra'))
-        else:
-            return redirect(url_for('solicitacoesCompra'))
-    else:
-        return redirect(url_for("login"))
+    status = request.form['status']
+    solicitacao_service.editarStatusSolicitacao(status, id)
+    return redirect(url_for('solicitacoesCompra'))
+
 
 #Página de cadastro de setores
 @app.route('/cadastrarSetor', methods=['GET', 'POST'])
+@autenticacao_service.login_required
+@autenticacao_service.nivel_required('administrador')
 def cadastrarSetor():
-    if "user_id" in session:
-        if session['nivel'] == "administrador":
-            if request.method == "POST":
-                nome = request.form["nome"]
-                solicitacao_service.cadastrar_setor(nome)
-            return render_template("cadastrarSetor.html", setor=None, action_url=url_for('cadastrarSetor'))
-        else:
-            return redirect(url_for('solicitacoesCompra'))
-    else:
-        return redirect(url_for("login"))
+    if request.method == "POST":
+        nome = request.form["nome"]
+        solicitacao_service.cadastrar_setor(nome)
+    return render_template("cadastrarSetor.html", setor=None, action_url=url_for('cadastrarSetor'))
+
 
 @app.route('/excluirSetor/<int:id>')
+@autenticacao_service.login_required
+@autenticacao_service.nivel_required('administrador')
 def excluirSetor(id):
-    if "user_id" in session:
-        if session['nivel'] == "administrador":
-            solicitacao_service.excluir_setor(id)
-            return redirect(url_for("setores"))
-        else:
-            return redirect(url_for('solicitacoesCompra'))
-    else:
-        return redirect(url_for("login"))
+    solicitacao_service.excluir_setor(id)
+    return redirect(url_for("setores"))
 
 @app.route('/setores')
+@autenticacao_service.login_required
 def setores():
-    if "user_id" in session:
-        setores = solicitacao_service.buscar_setores()[::-1] #inverte a lista
-        return render_template('setores.html', setores = setores, nome=session['user_nome'])
-    else:
-        return redirect(url_for("login"))
+    setores = solicitacao_service.buscar_setores()[::-1] #inverte a lista
+    return render_template('setores.html', setores = setores, nome=session['user_nome'])
+
 
 @app.route('/editarSetor/<int:id>')
+@autenticacao_service.login_required
+@autenticacao_service.nivel_required('administrador')
 def editarSetor(id):
-    if "user_id" in session:
-        if session['nivel'] == "administrador":
-            setor_resultado = solicitacao_service.buscar_setores(id, unica=True)
-            setor = setor_resultado[0] if setor_resultado else None
-            return render_template('cadastrarSetor.html', setor=setor, action_url=url_for('gravaEditarSetor', id=id))
-        else:
-            return redirect(url_for('solicitacoesCompra'))
-    else:
-        return redirect(url_for("login"))
+    setor_resultado = solicitacao_service.buscar_setores(id, unica=True)
+    setor = setor_resultado[0] if setor_resultado else None
+    return render_template('cadastrarSetor.html', setor=setor, action_url=url_for('gravaEditarSetor', id=id))
+
 
 @app.route('/gravaEditarSetor/<int:id>', methods=['POST'])
+@autenticacao_service.login_required
+@autenticacao_service.nivel_required('administrador')
 def gravaEditarSetor(id):
-    if "user_id" in session:
-        if session['nivel'] == "administrador":
-            if request.method == "POST":
-                nome = request.form["nome"]
-                solicitacao_service.editar_setor(id,nome)
-            return render_template("cadastrarSetor.html")
-        else:
-            return redirect(url_for('solicitacoesCompra'))
-    else:
-        return redirect(url_for("login"))
-
-
-
+    if request.method == "POST":
+        nome = request.form["nome"]
+        solicitacao_service.editar_setor(id,nome)
+    return render_template("cadastrarSetor.html")
 
 #Executa a aplicação
 if __name__ == '__main__':
