d60db92ec73d7f45ba5623d0927562edf3086773
DavidSilvaProg <davidsilva.prog@gmail.com>
2025-05-25 15:27:34 -0300

    ajuste de resposividade no formulario de cadastroSolicitacao.html

--- app/static/js/cadastroSolicitacao.js
+++ app/static/js/cadastroSolicitacao.js
@@ -11,7 +11,7 @@ function adicionarInput() {
     container.classList.add('flex', 'space-x-4', 'mb-4'); // Espaçamento e margem entre grupos
 
     container.innerHTML = `
-        <div class="flex-1">
+        <div class="flex-1 min-w-[150px]" >
             <label for="produto${contador}" class="block text-sm font-medium text-gray-700 mb-1">Produto ${contador}:</label>
             <input
                 type="text"
@@ -20,7 +20,7 @@ function adicionarInput() {
                 class="w-full border border-gray-300 rounded px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
             >
         </div>
-        <div class="w-28">
+        <div class="w-16">
             <label for="quantidade${contador}" class="block text-sm font-medium text-gray-700 mb-1">Quantidade:</label>
             <input
                 type="text"
